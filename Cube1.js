0b81aec915de78854c2385b21a83c77e6fce905c
akaiinc <stormahh@outlook.com>
2019-04-14 11:27:58 +0800

    使正方体立起来

--- Cube1.js
+++ Cube1.js
@@ -9,6 +9,12 @@ var vertexColors=[];
 var vertices=[];
 var theta=[0,0,0];
 var thetaLoc;
+var standardMatrix= mat4(
+    vec4(1,0,0,0),
+    vec4(0,1,0,0),
+    vec4(0,0,1,0),
+    vec4(0,0,0,1)
+);
 
 window.onload= function main(){
 
@@ -26,25 +32,36 @@ window.onload= function main(){
 
     var r=0.4;
     vertices = [
-        vec3(r,r,r),
-        vec3(-r,r,r),
-        vec3(-r,-r,r),
-        vec3(r,-r,r),
-        vec3(r,r,-r),
-        vec3(-r,r,-r),
-        vec3(-r,-r,-r),
-        vec3(r,-r,-r)
+        vec4(r,r,r,1.0),
+        vec4(-r,r,r,1.0),
+        vec4(-r,-r,r,1.0),
+        vec4(r,-r,r,1.0),
+        vec4(r,r,-r,1.0),
+        vec4(-r,r,-r,1.0),
+        vec4(-r,-r,-r,1.0),
+        vec4(r,-r,-r,1.0)
     ];
 
+    var rotY=standardMatrix;
+    rotY=mult(rotY,rotateZ(45));
+    var c=Math.sqrt(3)/3;
+    var radian=Math.asin(c);
+    var angle=(radian/(2*Math.PI))*360;
+    rotY=mult(rotY,rotateX(angle));
+
+    for(var i=0;i<vertices.length;i++){
+        vertices[i]=mult(rotY,vertices[i]);
+    }
+    
 
     vertexColors=[
         vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
-        vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
-        vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
-        vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
-        vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
-        vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
-        vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+        vec4( 1.0, 21/255, 100/255, 1.0 ),  // red
+        vec4( 245/255, 1.0, 90/255, 1.0 ),  // yellow
+        vec4( 13/255, 1.0, 165/255, 1.0 ),  // green
+        vec4( 100/255, 149/255, 237/255, 1.0 ),  // blue
+        vec4( 1.0, 13/255, 166/255, 1.0 ),  // magenta
+        vec4( 0.0, 191/255, 1.0, 1.0 ),  // cyan
         vec4( 1.0, 1.0, 1.0, 1.0 )   // white
     ];
     
@@ -75,7 +92,7 @@ function linkShader(pointsArray,colorsArray)
           gl.STATIC_DRAW );
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
 
@@ -100,7 +117,7 @@ function quad(a,b,c,d){
     var indices=[a,b,c,a,c,d];
     for(var i=0;i<indices.length;i+=1){
         points.push(vertices[indices[i]]);
-        colors.push(vertexColors[indices[i]]);
+        colors.push(vertexColors[a]);
     }
 }
 
@@ -108,9 +125,8 @@ function quad(a,b,c,d){
 function cubeRender(){
 
     gl.clear( gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    theta[0]+=2.0;
-    theta[1]+=4.0;
-    theta[2]+=6.0;
+
+    theta[1]+=2.0;
     gl.uniform3fv(thetaLoc,flatten(theta));
     gl.drawArrays( gl.TRIANGLES, 0, points.length );
     //绘制方式、起始点、点的个数
